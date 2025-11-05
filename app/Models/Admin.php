e1812ba68b5d5a2c1d1a3f44660ad0ba5e0a33c2
AyeshaAslam1234 <mohra302010@gmail.com>
2025-11-05 17:04:38 +0500

    i do not know

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -27,7 +27,7 @@ class Admin extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * The attributes that should be hidden for serialization.  
      *
      * @var list<string>
      */
